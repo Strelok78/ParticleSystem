48dab91c5be3bc2cea9bed33f7795fedff0005b1
Shota <eg.shota@yandex.ru>
2023-07-06 11:33:14 +0300

    Git_repository_created

---
+++ ParticleSystemLesson/Assets/Scripts/Cursor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    public float Speed = 8.0f;
+    public float DistanceFromCamera = 5.0f;
+
+    void Update()
+    {
+        Vector3 MousePosition = Input.mousePosition;
+        MousePosition.z = DistanceFromCamera;
+
+        Vector3 MouseScreenToWorld = Camera.main.ScreenToWorldPoint(MousePosition);
+
+        Vector3 Position = Vector3.Lerp(transform.position, MouseScreenToWorld, 1.0f - Mathf.Exp(-Speed * Time.deltaTime));
+
+        transform.position = Position;
+    }
+}
